4dd7e05bce1f600942719db41d150cd85b58c6b5
BeeKeeper-maker <faysalhamed1998@gmail.com>
2025-08-27 20:45:20 +0600

    Add .next/static/chunks/src_app_layout_tsx_c0237562._.js 1756305918060

---
+++ .next/static/chunks/src_app_layout_tsx_c0237562._.js
@@ -0,0 +1,9 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/[root-of-the-server]__9a168d68._.css",
+  "static/chunks/node_modules_9ddc48cb._.js",
+  "static/chunks/src_components_cc7f7465._.js"
+],
+    source: "dynamic"
+});
